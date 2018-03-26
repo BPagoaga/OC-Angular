db691b1455624be0ba474cad2d3d482476707d6f
Bernard Pagoaga <bernard.pagoaga@webdealauto.com>
2018-03-26 10:06:15 +0200

    [UPDATE] Increase/decrease methods, @todo : display the date

--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { Post } from "../post";
 
 @Component({
   selector: "app-post-list-item",
@@ -6,9 +7,19 @@ import { Component, Input, OnInit } from "@angular/core";
   styleUrls: ["./post-list-item.component.scss"]
 })
 export class PostListItemComponent implements OnInit {
-  @Input() post: Object;
+  @Input() post: Post;
 
   constructor() {}
 
   ngOnInit() {}
+
+  increaseLike() {
+    this.post.loveIts += 1;
+  }
+
+  decreaseLike() {
+    if (this.post.loveIts !== 0) {
+      this.post.loveIts -= 1;
+    }
+  }
 }
